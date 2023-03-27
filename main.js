ee9c2f0d0958753824539a88cb2d60bf51884a5d
Daniel Morales <dmorales@heads-up.com>
2023-03-27 11:36:58 -0500

    jh

--- main.js
+++ main.js
@@ -17,6 +17,18 @@ recognition.addEventListener('result', (e)=>{
     p.innerText = text;
     texts.appendChild(p);
     if(e.results[0].isFinal){
+        if(text.includes('shades down') || text.includes('lower shade')){
+            shades_down();
+        }
+        if(text.includes('shades up') || text.includes('raise shade')){
+            shades_up();
+        }
+        if(text.includes('lights on') || text.includes('turn on lights')){
+            Light_on();
+        }
+        if(text.includes('lights off') || text.includes('turn off lights')){
+            Light_off();
+        }
         p = document.createElement('p');
     }
     console.log(text);
@@ -25,4 +37,20 @@ recognition.addEventListener('result', (e)=>{
 recognition.addEventListener('end',()=>{
     recognition.start()
 })
+function Light_on(){
+    console.log('hello');
+    document.getElementById('Light').src = "Daco_on.png";
+}
+function Light_off(){
+    console.log('hello');
+    document.getElementById('Light').src = "Daco.png";
+}
+function shades_down(){
+    console.log('hello');
+    document.getElementById('Shade').src = "shadeDown.png";
+}
+function shades_up(){
+    console.log('hello');
+    document.getElementById('Shade').src = "shadeUp.png";
+}
 recognition.start();
